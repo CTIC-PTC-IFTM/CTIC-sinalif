7f2030ac37eeb279b535ee17b2dc0cb40fa12a2d
JoaoAugusto2020 <joaoaugustoptc2020@gmail.com>
2025-08-22 11:50:35 -0300

    fix(autostart): remove admin role

--- recursos/scripts/autostart.bat
+++ recursos/scripts/autostart.bat
@@ -1,27 +1,6 @@
 @echo off
 title Sinal IF
 
-:: ================================================================
-:: Verificacao e Solicitacao de Privilegios de Administrador
-:: ================================================================
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    echo Solicitando privilegios de administrador...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
-    cls
-
 echo ==========================================================
 echo                        Sinal IF
 echo ==========================================================
